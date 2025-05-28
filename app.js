f2d0b0806af86410929fab140d8b2a58bf61f882
Marco Braga <maboa17@gmail.com>
2025-05-28 19:39:39 -0300

    refactor(feature/home): update app.js

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3002;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!\n');
+    if (req.url === '/' || req.url === '/home.html') {
+        const filePath = path.join(__dirname, 'home.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Erro ao carregar o arquivo.');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/home.css') {
+        const cssPath = path.join(__dirname, 'home.css');
+        fs.readFile(cssPath, (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Erro ao carregar o CSS.');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/css');
+                res.end(data);
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Página não encontrada.');
+    }
 });
 
 server.listen(port, hostname, () => {
